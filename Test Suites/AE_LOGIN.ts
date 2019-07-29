06277c29dbd98ee2e6fcc362a77ba360dcd59d56
yangming <yangming@bw30.com>
2019-07-29 11:09:27 +0800

    grid

---
+++ Test Suites/AE_LOGIN.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>AE_LOGIN</description>
+   <name>AE_LOGIN</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>71b3b04d-7fd7-44fd-bfc6-8871b625d630</testSuiteGuid>
+   <testCaseLink>
+      <guid>7cf17115-1c7b-4425-bf0e-12aae406367a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AE4.0/AE_login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/AE_LOGIN_COLL.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description>AE_LOGIN_COLL</description>
+   <name>AE_LOGIN_COLL</name>
+   <tag>AE_LOGIN_COLL</tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/AE_LOGIN</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
